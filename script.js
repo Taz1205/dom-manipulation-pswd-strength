25ff383ecd2a0dcdd052b1831ef35b46fd441e6d
Fatima Zahra <fathima_z@yahoo.com>
2023-07-07 14:10:49 +0100

    Added condns: capitals, lowercase, nums, spl chars

--- script.js
+++ script.js
@@ -7,16 +7,30 @@ passwordInput.addEventListener("input", updateProgressBar);
 function updateProgressBar() {
   const strength = calculateStrength(passwordInput.value);
   progressFill.style.width = `${strength}%`;
+  console.log(passwordInput.value.length);
 
-  if (passwordInput.value.length < 8 && strength <= 33) {
+  if (passwordInput.value.length < 8 && strength <= 40) {
     progressFill.style.backgroundColor = "red";
     feedback.textContent = "Should be longer";
-  } else if (strength <= 66 && passwordInput.value.length < 12) {
+  } else if (strength <= 70 && passwordInput.value.length < 12) {
     progressFill.style.backgroundColor = "orange";
     feedback.textContent = "Pretty good";
   } else {
     progressFill.style.backgroundColor = "limegreen";
     feedback.textContent = "Grrrreat!";
+  }
+  checkForStrongPassword(passwordInput.value);
+}
+
+function checkForStrongPassword(password) {
+  if (
+    !/[A-Z]/.test(password) ||
+    !/[a-z]/.test(password) ||
+    !/[0-9]/.test(password) ||
+    !/[\W]/.test(password)
+  ) {
+    feedback.textContent +=
+      " ,to make your password stronger, include a mix of upper-case, lower-case letters, numbers and spl characters";
   }
 }
 
